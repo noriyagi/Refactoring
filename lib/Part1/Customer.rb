9d6865159cab7279246692988e3b6bceb510d13d
Noriaki Koyanagi <koyanagn@gmail.com>
2011-10-30 18:31:05 +0900

    1.4 1

--- lib/Part1/Customer.rb
+++ lib/Part1/Customer.rb
@@ -39,19 +39,7 @@ class Customer
 
   #ここからメソッド抽出
   def amount_for(rental)
-    result = 0
-
-  #各行の金額を計算
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
+    rental.charge
   end
     
 end
